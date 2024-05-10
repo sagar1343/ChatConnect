084f6939823d0a31635044cfdb7db65827fc19d4
Sagar <sagarchakrawarti24@gmail.com>
2024-05-11 01:28:54 +0530

    fixes banner image

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,6 +1,11 @@
 import banner from '../assets/banner.svg';
 const Banner = () => {
-  return <img src={banner} />;
+  return (
+    <img
+      width='100%'
+      src={banner}
+    />
+  );
 };
 
 export default Banner;
