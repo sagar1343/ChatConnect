4f05235c688584533032f331ffaf6853d7031572
Sagar <sagarchakrawarti24@gmail.com>
2024-05-11 00:20:48 +0530

    added event handler in message input field

--- frontend/src/components/ChatDetails.jsx
+++ frontend/src/components/ChatDetails.jsx
@@ -1,29 +1,42 @@
 import { TextField, Fab, Stack } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
+import { useRef } from 'react';
 const ChatDetails = () => {
+  const messageRef = useRef(null);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // update db
+    console.log(messageRef.current.value);
+    messageRef.current.value = '';
+  };
   return (
-    <Stack
-      direction='row'
-      width='66%'
-      justifyContent='space-between'
-      position='fixed'
-      bottom='1rem'
-    >
-      <TextField
-        color='success'
-        sx={{
-          bgcolor: '#fff',
-          flexGrow: 1,
-        }}
-        placeholder='Message'
-      ></TextField>
-      <Fab
-        sx={{ marginInline: '1rem' }}
-        color='success'
+    <form onSubmit={(event) => handleSubmit(event)}>
+      <Stack
+        direction='row'
+        width='66%'
+        justifyContent='space-between'
+        position='fixed'
+        bottom='1rem'
       >
-        <SendIcon />
-      </Fab>
-    </Stack>
+        <TextField
+          inputRef={messageRef}
+          required
+          color='success'
+          sx={{
+            bgcolor: '#fff',
+            flexGrow: 1,
+          }}
+          placeholder='Message'
+        ></TextField>
+        <Fab
+          type='submit'
+          sx={{ marginInline: '1rem' }}
+          color='success'
+        >
+          <SendIcon />
+        </Fab>
+      </Stack>
+    </form>
   );
 };
 
