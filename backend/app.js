f4883d9bc8d646de12c92f15f20cc89ad94fd8e6
Sagar <sagarchakrawarti24@gmail.com>
2024-05-20 13:26:56 +0530

    allow cors

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
-
+import cors from 'cors'
 import { createServer } from 'http';
 import express from 'express';
 import { Server } from 'socket.io';
@@ -17,6 +17,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
+app.use(cors())
 app.use(morgan('tiny'));
 app.use(express.json({ limit: '10mb' }));
 app.use('/chatconnect/api/auth', authRouter)
